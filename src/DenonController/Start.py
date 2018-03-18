ae1fce92bd9e163ca41215abbda86ca98e610e6e
Jens Larsen <mr.javawolf@gmail.com>
2018-03-18 20:44:31 +0100

    Added: Can accept commandline arguments and will execute the argument in a non-blocking way

--- src/DenonController/Start.py
+++ src/DenonController/Start.py
@@ -1,3 +1,4 @@
+import sys
 import DenonCommands
 from MainLoop import MainLoop
 from UserConsoleInput import UserConsoleInput
@@ -20,9 +21,26 @@ def GetCommands(denon):
 print("Welcome to JWolf's Denon controls!")
 denon = DenonConnection(Denon_IP, Denon_Port)
 commands = GetCommands(denon)
-userInputToCommands = TranslateUserInputToCommands(commands)
-userInput = UserConsoleInput()
-mainLoop = MainLoop()
-mainLoop.Start(userInput, userInputToCommands)
+translateUserInputToCommands = TranslateUserInputToCommands(commands)
 
+#If there is no commandline arguments, we will start the mainloop and be in a "interactive mode"
+if len(sys.argv) == 1:
+    userInput = UserConsoleInput()
+    mainLoop = MainLoop()
+    mainLoop.Start(userInput, userInputToCommands)
+
+#If there are arguments we will execute the argument and return to the commandline in a non-blocking way
+else:
+    translateUserInputToCommands.AddInput(sys.argv[1])
+    if translateUserInputToCommands.IsCommand():
+        command = translateUserInputToCommands.GetCommand()
+        fullUserInput = translateUserInputToCommands.GetUserInputs()
+        try:
+            command.Execute(fullUserInput)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            print(traceback.format_exc())
+    else:
+        print("Unknown argument: '"+sys.argv[1]+"'")
 denon.Close()
