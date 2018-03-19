57a33c27024a3dfb85a8d3267f8de2bcc4690615
Jens Larsen <mr.javawolf@gmail.com>
2018-03-19 19:59:28 +0100

    Updated: I2C read/write

--- src/DenonController/UserI2CInput.py
+++ src/DenonController/UserI2CInput.py
@@ -1,18 +1,21 @@
 #http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
 import smbus
+import sys
 
 #class UserI2CInput(object):
 
 #     def Read(self):
 print("Setting up the bus...")
 bus = smbus.SMBus(1) 
-DEVICE_ADDRESS = 0x38      #7 bit address (will be left shifted to add the read write bit)
-READ_FIRST_ROW_VALUE = 0x40
+DEVICE_ADDRESS = 0x20      #7 bit address (will be left shifted to add the read write bit)
+READ_FIRST_ROW_VALUE = 0x1
+if len(sys.argv) == 2:
+    READ_FIRST_ROW_VALUE = sys.argv[1]
 
-print("writes the read byte...")
+print("writes the read byte:" + READ_FIRST_ROW_VALUE)
 #Write a single register
 bus.write_byte(DEVICE_ADDRESS, READ_FIRST_ROW_VALUE)
 
-print("reads the read byte...")
+print("reads the read result...")
 x = bus.read_byte(DEVICE_ADDRESS, READ_FIRST_ROW_VALUE)
-print("Read: "+x)
+print("Read: " + x)
