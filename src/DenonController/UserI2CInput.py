22e85a8b69923f6534bd4653c34eee5f1ef7a63f
Jens Larsen <mr.javawolf@gmail.com>
2018-03-19 20:18:11 +0100

    Updated: Reads all the I2C values

--- src/DenonController/UserI2CInput.py
+++ src/DenonController/UserI2CInput.py
@@ -1,21 +1,28 @@
 #http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
 import smbus
 import sys
+import math
 
 #class UserI2CInput(object):
 
 #     def Read(self):
+I2C_BUS = 1
+DEVICE_ADDRESS = 0x20 #7 bit address (will be left shifted to add the read write bit)
+w, h = 4, 4
 print("Setting up the bus...")
-bus = smbus.SMBus(1) 
-DEVICE_ADDRESS = 0x20      #7 bit address (will be left shifted to add the read write bit)
-READ_FIRST_ROW_VALUE = 0x1
-if len(sys.argv) == 2:
-    READ_FIRST_ROW_VALUE = sys.argv[1]
+bus = smbus.SMBus(I2C_BUS_NUMBER) 
+Matrix = [[0 for x in range(w)] for y in range(h)]
+result_x[4]
+result_y[4]
+for i in range(4):
+    row_to_read = math.pow(2, i)
+    bus.write_byte(DEVICE_ADDRESS, row_to_read)
+    result_x[i] = bus.read_byte(DEVICE_ADDRESS)
+for i in range(4):
+    row_to_read = math.pow(2, i + 4)
+    bus.write_byte(DEVICE_ADDRESS, row_to_read)
+    result_y[i] = bus.read_byte(DEVICE_ADDRESS)
 
-print("writes the read byte:" + READ_FIRST_ROW_VALUE)
-#Write a single register
-bus.write_byte(DEVICE_ADDRESS, READ_FIRST_ROW_VALUE)
+print("xvalues: " + ", ".join(result_x))
+print("yvalues: " + ", ".join(result_y))
 
-print("reads the read result...")
-x = bus.read_byte(DEVICE_ADDRESS, READ_FIRST_ROW_VALUE)
-print("Read: " + x)
