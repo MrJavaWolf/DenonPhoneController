9df648d20c3a75d860577c300f72c30671372c91
Jens Larsen <mr.javawolf@gmail.com>
2018-03-19 20:54:03 +0100

    Updated: Can read all the I2C keys

--- src/DenonController/UserI2CInput.py
+++ src/DenonController/UserI2CInput.py
@@ -2,27 +2,43 @@
 import smbus
 import sys
 import math
+import time
+
+def ReadI2CInput(bus):
+    xRow = []
+    yRow = []
+    for i in range(3, -1, -1):
+        row_to_read = int(math.pow(2, i))
+        bus.write_byte(DEVICE_ADDRESS, row_to_read)
+        xRow.append(bus.read_byte(DEVICE_ADDRESS))
+    for i in range(4):
+        row_to_read = int(math.pow(2, i + 4))
+        bus.write_byte(DEVICE_ADDRESS, row_to_read)
+        yRow.append(bus.read_byte(DEVICE_ADDRESS))
+
+    inputMatrix = [[0 for y in range(4)] for x in range(4)]
+    for y in range(4):
+        for x in range(4):
+            if xRow[x] == 0 and yRow[y] == 0:
+                inputMatrix[y][x] = int(1)
+    return inputMatrix
 
 #class UserI2CInput(object):
 
 #     def Read(self):
 I2C_BUS = 1
 DEVICE_ADDRESS = 0x20 #7 bit address (will be left shifted to add the read write bit)
-w, h = 4, 4
 print("Setting up the bus...")
 bus = smbus.SMBus(I2C_BUS) 
-Matrix = [[0 for x in range(w)] for y in range(h)]
-result_x = []
-result_y = []
-for i in range(4):
-    row_to_read = math.pow(2, i)
-    bus.write_byte(DEVICE_ADDRESS, row_to_read)
-    result_x.append(bus.read_byte(DEVICE_ADDRESS))
-for i in range(4):
-    row_to_read = math.pow(2, i + 4)
-    bus.write_byte(DEVICE_ADDRESS, row_to_read)
-    result_y.append(bus.read_byte(DEVICE_ADDRESS))
+while True:
+    
+    inputMatrix = ReadI2CInput(bus)
+    print("- - - - -")
+    for y in range(len(inputMatrix)):
+        for x in range(len(inputMatrix[y])):
+            print(str(inputMatrix[y][x]) + " ", end = '')
+        print("")
+    
+    time.sleep(0.1)
 
-print("xvalues: " + ", ".join(result_x))
-print("yvalues: " + ", ".join(result_y))
 
