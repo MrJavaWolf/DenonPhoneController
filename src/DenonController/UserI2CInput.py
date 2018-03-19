1c0c5a2ae1bff20ea40119c2c3581fec66c5a789
Jens Larsen <mr.javawolf@gmail.com>
2018-03-19 20:22:02 +0100

    Bugfix: Arrays

--- src/DenonController/UserI2CInput.py
+++ src/DenonController/UserI2CInput.py
@@ -12,16 +12,16 @@ w, h = 4, 4
 print("Setting up the bus...")
 bus = smbus.SMBus(I2C_BUS) 
 Matrix = [[0 for x in range(w)] for y in range(h)]
-result_x[4]
-result_y[4]
+result_x = []
+result_y = []
 for i in range(4):
     row_to_read = math.pow(2, i)
     bus.write_byte(DEVICE_ADDRESS, row_to_read)
-    result_x[i] = bus.read_byte(DEVICE_ADDRESS)
+    result_x.append(bus.read_byte(DEVICE_ADDRESS))
 for i in range(4):
     row_to_read = math.pow(2, i + 4)
     bus.write_byte(DEVICE_ADDRESS, row_to_read)
-    result_y[i] = bus.read_byte(DEVICE_ADDRESS)
+    result_y.append(bus.read_byte(DEVICE_ADDRESS))
 
 print("xvalues: " + ", ".join(result_x))
 print("yvalues: " + ", ".join(result_y))
